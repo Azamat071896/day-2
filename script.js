51b46cceba6d74b4c29e41fb9a49724ef1da9ea6
Azamat <Azamat>
2021-09-24 01:20:15 +0600

    init

---
+++ script.js
@@ -0,0 +1,102 @@
+//Begin16
+// let x1 = 12
+// let x2 = 56
+// let c = x2 - x1
+// console.log(c);
+
+//Begin17
+// let A = 4
+// let B = 5
+// let C = 10
+
+//Begin18
+// let s = C - A
+// let f = C - B
+// let h = s + f
+// console.log(h);
+
+//Begin19
+// let A = 4
+// let B = 10
+// let C = 5
+
+//Begin20
+// let s = C - A
+// let f = C - B
+// let h = s * f
+// console.log(h);
+
+//Begin21
+// let x1 = 1
+// let y1 = 2
+// let x2 = 6
+// let y2 = 7 
+// let s =  x2 - x1 + x2 - x1 + y2 - y1 + y2 - y1
+// let v = (x2 - x1) * (y2 - y1)
+// console.log(s);
+// console.log(v);
+
+//Begin22
+// let x1 = 1
+// let y1 = 2
+// let x2 = 6
+// let y2 = 7 
+// let s =  (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)
+// console.log(s);
+
+//Begin23
+// let x1 = 2
+// let y1 = 2
+// let x2 = 6
+// let y2 = 7
+// let x3 = 9
+// let y3 = 2
+
+//Begin24
+// let a = (x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1)
+// let b = (x3 - x2)*(x3 - x2)+(y3 - y2)*(y3 - y2)
+// let c = (x1- x3)*(x1 - x3)+(y1 - y3)*(y1 - y3)
+// let p = a+b+c/2
+// let S = p*(p-a)*(p-b)*(p-c)
+// let h = S*S
+// console.log(h);
+
+//Begin25
+// let x = 4
+// let y = 3*x*x*x*x*x*x - 6*x*x - 7
+
+// console.log(`Значение ${y}`);
+
+
+//Begin26
+// let x = 9
+// let y = 4*(x-3)*(x-3)*(x-3)*(x-3)*(x-3)*(x-3) - 7*(x-3)*(x-3)*(x-3)+2
+// console.log(`Значение: ${y}`);
+
+//Begin27
+// let A = 4
+
+// let A2 = A*A
+// let A4 = A2*A2
+// let A8 = A4*A4
+// console.log(`Значение:A ${A2} Значение:A2 ${A4} Значение:A4 ${A8}`);
+
+//Begin28
+// let A = 4
+
+// let A2 = A*A
+// let A3 = A2*A2
+// let A5 = A3*A3
+// let A10 = A5*A5
+// let A15 = A10*A10
+// console.log(`Значение:A ${A2} Значение:A2 ${A3} Значение:A3 ${A5} Значение:A5 ${A10} Значение:A10 ${A15}`);
+
+//Begin29
+// let a = 82
+// let b = 3.14*a/180
+// console.log(b)
+
+//Begin30
+// let a = 45
+// let b = 180*a/3.14
+// console.log(b);
